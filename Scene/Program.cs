08185275c5216b2245448e42fe05fdc2d1757341
ShamLamKek <csgo.sham@yandex.ru>
2020-04-13 02:10:25 +0800

    init

--- Scene/Program.cs
+++ Scene/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Scene
 {
@@ -25,5 +26,75 @@ namespace Scene
             }
 
         }
+        public static void choose(params Action[] scenes)
+        {
+            
+            Dictionary<ConsoleKey, Action> keys = new Dictionary<ConsoleKey, Action>
+            {
+                { ConsoleKey.D1, scenes[0] },
+                {ConsoleKey.D2, scenes[1] },
+                {ConsoleKey.D3, scenes[2] },
+                {ConsoleKey.D4, scenes[3] }
+            };
+            var key = Console.ReadKey();
+            Action action;
+
+            if (keys.TryGetValue(key.Key, out action))
+            {
+                action();
+            }
+
+        }
+        public static void choose1(params Action[] scenes)
+        {
+
+            var dictionary = new Dictionary<Int32, Action>();
+            var iter = 0;
+            foreach (var scene in scenes)
+            {
+                dictionary[iter] = scene;
+                iter++;
+            }
+            var key = Int32.Parse(Console.ReadLine());
+            foreach (KeyValuePair<Int32, Action> kvp in dictionary)
+            {
+                if (key == iter)
+                {
+                    kvp.Value();
+                }
+
+            }
+
+        }
+        //static void scene4()
+        //{
+        //    jump();
+        //    q = "вова позавтракал. на часах 7:30." +
+        //     space +
+        //     "на чем вова будете добираться до школы?";
+        //    a1 = "-на автобусе(займет 30 минут)";
+        //    a2 = "-пешком(займет час)";
+        //    a3 = "-на велосипеде(займет 10 минут)";
+        //    scene(line, q, a1, a2, a3);
+        //}
+        //static void scene8()
+        //{
+        //    jump();
+        //    Q = "Вова решил поехать на автобусе и приехал ровно в 8:00. Поздравляю! вова успели в школу";
+        //    scene(Line, Q);
+        //}
+        //static void scene9()
+        //{
+        //    jump();
+        //    Q = "вова решил пойти пешком и опоздал в школу на полчаса. к сожалениию, вова опоздали, завтра вове придется встать пораньше. ";
+        //    scene(Line, Q);
+        //}
+        //static void scene10()
+        //{
+        //    jump();
+        //    Q = "вова решил поехать на велосипеде и успел в школу. оздравляю! вова успели в школу";
+        //    scene(Line, Q);
+        //}
+
     }
 }
--- TreeDialog/Program.cs
+++ TreeDialog/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace ConsoleApplication8
@@ -17,43 +18,148 @@ namespace ConsoleApplication8
         public static string A6;
         public static string A8; 
         public static string A9;
+        public static Action action;
+
         public static void Main()
 
         {
             scenemain();
-            choose(scene1, scene2);
-
-        }    
+            choose(scene1, scene2, scene4);
+            if (action == scene1)
+            {
+                choose(scene6, scene5);
+                if (action == scene5)
+                {
+                    choose(scene6);
+                }
+                choose(scene7);
+                choose(scene8, scene9, scene10);
+            }
+            else if (action == scene2)
+            {
+                choose(scene11, scene7, scene7, scene4);
+                if (action == scene11)
+                {
+                    choose(scene12, scene13, scene10);
+                }
+                else if (action == scene7)
+                {
+                    choose(scene8, scene9, scene10);
+                }
+            }
+        }
 
 
         static void scenemain()
-        { 
-            Q = "-Как дела?";
-            A1 = "-Хорошо"; 
-            A2 = "-Плохо";
-            scene(Line, Q, A1, A2);
+        {
+            Q = "Вова школьник и каждый день он ходит в школу к 8:00. Вова просыпается и..  ";
+            A1 = "-Сразу же встает.";
+            A2 = "-Решает полежать еще полчасик.";
+            A3 = "-Решает спать дальше и никуда не идти.";
+            scene(Line, Q, A1, A2, A3);
         }
         static void scene1()
         {
-            jump("-Печально");
-            Q = "Что случилось?";
-            A1 = "-Неважно";
-            A2 = "-Да так";
+            jump();
+            Q = "Вова встал. Что дальше?";
+            A1 = "-Вова идет умываться.";
+            A2 = "-Вова идет завтракать.";
             scene(Line, Q, A1, A2);
+            
         }
         static void scene2()
         {
-            jump("-здорово!");
-            Q = "Я рад!";
-            A1 = "-Пока";
-            A2 = "-Пока!!!!!";
+            jump();
+            Q = "Вова полежал полчасика и его время ограничено. Что дальше?";
+            A1 = "-Вова встает, умывается и идет завтракать.";
+            A2 = "-Вова встает, умывается, но не идет завтракать.";
+            A3 = "-Вова встает, завтракает, но не идет умываться.";
+            A4 = "-Вова решает спать дальше.";
+            scene(Line, Q, A1, A2,A3, A4);
+
+        }
+        static void scene3()
+        {
+            jump();
+            Q = "Вова полежал часик. Что дальше?";
+            A1 = "-Вова встает.";
+            A2 = "-Вова решает спать дальше.";
             scene(Line, Q, A1, A2);
+        }
+        static void scene4()
+        {
+            jump();
+            Q = "Вова решает спать дальше. Вова проспал все уроки.";
+            scene(Line, Q);
+        }
+
+        static void scene5()
+        {
+            jump();
+            Q = "Вова решил пойти завтракать.Но мама сказала сначала пойти умыться.";
+            A1 = "-Вова идет умываться.";
+            scene(Line, Q, A1);
+        }
+        static void scene6()
+        {
+            jump();
+            Q = "Вова умылся, теперь можно и позавтракать.";
+            A1 = "-Завтракать";
+            scene(Line, Q, A1);
+        }
+        static void scene7()
+        {
+            jump();
+            Q = "Вова позавтракал. На часах 7:30. На чем Вова будете добираться до школы?";
+            A1 = "-На автобусе(займет 30 минут)";
+            A2 = "-Пешком(займет час)";
+            A3 = "-На велосипеде(займет 10 минут)";
+            scene(Line, Q, A1, A2, A3);
+        }
+        static void scene8()
+        {
+            jump();
+            Q = "Вова решил поехать на автобусе и приехал ровно в 8:00. Поздравляю! Вова успели в школу";
+            scene(Line, Q);
+        }
+        static void scene9()
+        {
+            jump();
+            Q = "Вова решил пойти пешком и опоздал в школу на полчаса. К сожалениию, Вова опоздали, завтра Вове придется встать пораньше. ";
+            scene(Line, Q);
+        }
+        static void scene10()
+        {
+            jump();
+            Q = "Вова решил поехать на велосипеде и успел в школу. Поздравляю! Вова успели в школу";
+            scene(Line, Q);
+        }
+        static void scene11()
+        {
+            jump();
+            Q = "Вова позавтракал и умылся. На часах 7:45. На чем Вова будете добираться до школы?";
+            A1 = "-На автобусе(займет 30 минут)";
+            A2 = "-Пешком(займет час)";
+            A3 = "-На велосипеде(займет 10 минут)";
+            scene(Line, Q, A1, A2, A3);
+        }
+        static void scene12()
+        {
+            jump();
+            Q = "Вова решил поехать на автобусе и опоздал в школу. К сожалениию, вова опоздали, завтра вове придется встать пораньше.";
+            scene(Line, Q);
+        }
+        static void scene13()
+        {
+            jump();
+            Q = "Вова решил пойти пешком и опоздал в школу. К сожалениию, вова опоздали, завтра вове придется встать пораньше. ";
+            scene(Line, Q);
         }
 
 
-        public static void jump(string output)
+
+        public static void jump()
         {
-            Console.WriteLine(output);
             Thread.Sleep(1500);
             Console.Clear();
         }
@@ -68,51 +174,19 @@ namespace ConsoleApplication8
         }
         public static void choose(params Action[] scenes)
         {
-            ConsoleKey key = Console.ReadKey().Key;
-            if (key== ConsoleKey.D1)
+            var key = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < scenes.Length; i++)
             {
-                scenes[0]();
+                if (key == i + 1 )
+                {
+                    action = scenes[i];
+                    scenes[i]();
+                    break;
+                }
+                    
+                
             }
-            
-            if (key == ConsoleKey.D2)
-            {
-                scenes[1]();
-            }
-            if (key == ConsoleKey.D3)
-            {
-                scenes[2]();
-            }
-            if (key == ConsoleKey.D3)
-            {
-                scenes[2]();
-            }
-            if (key == ConsoleKey.D4)
-            {
-                scenes[3]();
-            }
-            if (key == ConsoleKey.D5)
-            {
-                scenes[4]();
-            }
-            if (key == ConsoleKey.D6)
-            {
-                scenes[5]();
-            }
-            if (key == ConsoleKey.D7)
-            {
-                scenes[6]();
-            }
-            if (key == ConsoleKey.D8)
-            {
-                scenes[7]();
-            }
-            if (key == ConsoleKey.D9)
-            {
-                scenes[8]();
-            }
-
-        }
-        
+        }     
     }
 }
 
